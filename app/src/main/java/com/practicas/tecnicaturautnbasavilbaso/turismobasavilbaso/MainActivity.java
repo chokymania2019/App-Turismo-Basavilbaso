d6ee2d9f5c03642f3d8ddfdc5e9d106d08444ed6
GabrielNumaX <>
2019-02-07 16:29:05 -0300

    imageView bandera
    se le agrego la bandera y un textView para bienvenida

--- app/src/main/java/com/practicas/tecnicaturautnbasavilbaso/turismobasavilbaso/MainActivity.java
+++ app/src/main/java/com/practicas/tecnicaturautnbasavilbaso/turismobasavilbaso/MainActivity.java
@@ -2,8 +2,11 @@ package com.practicas.tecnicaturautnbasavilbaso.turismobasavilbaso;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView bandera;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,6 +16,14 @@ public class MainActivity extends AppCompatActivity {
         //icono en el action bar
         getSupportActionBar().setDisplayShowHomeEnabled(true);
         getSupportActionBar().setIcon(R.mipmap.ic_launcher);
+
+        //imageView bandera
+        bandera=(ImageView)findViewById(R.id.imageViewBandera);
+
+        int intBandera=getResources().getIdentifier("drawable/bandera basso app", null, this.getPackageName());
+        bandera.setImageResource(intBandera);
+
+
 
     }
 }
