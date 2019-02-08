474fa738a98193f9c33a97d111c560fc90f31be7
GabrielNumaX <>
2019-02-08 10:46:22 -0300

    Metodos Activities Botones Menu
    se agregaron los metodos para abrir los activities en los botones del menu

--- app/src/main/java/com/practicas/tecnicaturautnbasavilbaso/turismobasavilbaso/MainActivity.java
+++ app/src/main/java/com/practicas/tecnicaturautnbasavilbaso/turismobasavilbaso/MainActivity.java
@@ -1,17 +1,32 @@
 package com.practicas.tecnicaturautnbasavilbaso.turismobasavilbaso;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     ImageView bandera;
+    Button atractivos;
+    Button alojamientos;
+    Button gastronomia;
+    Button telefonos;
+    Button plano;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //casteo de los botones
+        atractivos=(Button)findViewById(R.id.btnAtractivos);
+        alojamientos=(Button)findViewById(R.id.btnAlojamientos);
+        gastronomia=(Button)findViewById(R.id.btnGastronomia);
+        telefonos=(Button)findViewById(R.id.btnTelefonos);
+        plano=(Button)findViewById(R.id.btnTelefonos);
 
         //icono en el action bar
         getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -26,4 +41,40 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
+
+    public void abrirAtractivos (View view){
+
+        Intent inAt = new Intent (this, AtractivosActivity.class);
+        startActivity(inAt);
+
+    }
+
+    public void abrirAlojamientos (View view){
+
+        Intent inAl = new Intent (this, AlojamientoActivity.class);
+        startActivity(inAl);
+
+    }
+
+    public void abrirGastronomia (View view){
+
+        Intent inGa = new Intent (this, GastronomiaActivity.class);
+        startActivity(inGa);
+
+    }
+
+    public void abrirTelefonos (View view){
+
+        Intent inTel = new Intent (this, TelUtilActivity.class);
+        startActivity(inTel);
+
+    }
+    public void abrirPlano (View view){
+
+        Intent inPla = new Intent (this, PlanoActivity.class);
+        startActivity(inPla);
+
+    }
+
+
 }
