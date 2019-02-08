1206c17beb1373ec207a45770928e2b149462474
GabrielNumaX <>
2019-02-08 09:56:34 -0300

    Activities Botones Menu Principal
    5 activities
    Atractivos
    Alojamiento
    Gastronomia
    TelUtil
    Plano

---
+++ app/src/main/java/com/practicas/tecnicaturautnbasavilbaso/turismobasavilbaso/AlojamientoActivity.java
@@ -0,0 +1,13 @@
+package com.practicas.tecnicaturautnbasavilbaso.turismobasavilbaso;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class AlojamientoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_alojamiento);
+    }
+}
