0538652143491e3883da8519542a7e5498e39589
GabrielNumaX <>
2019-02-18 10:26:57 -0300

    Activity Google Maps
    activity del boton plano de la ciudad que  va a google maps completo. falta probarlo y que abra el intent

--- app/src/main/java/com/practicas/tecnicaturautnbasavilbaso/turismobasavilbaso/PlanoActivity.java
+++ app/src/main/java/com/practicas/tecnicaturautnbasavilbaso/turismobasavilbaso/PlanoActivity.java
@@ -1,13 +1,34 @@
 package com.practicas.tecnicaturautnbasavilbaso.turismobasavilbaso;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class PlanoActivity extends AppCompatActivity {
+
+    ImageView plano;
+    Button googleMaps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_plano);
+
+        googleMaps=(Button)findViewById(R.id.btnGoogleMaps);
+
+        //imageView plano
+        plano=(ImageView)findViewById(R.id.imageViewPlano);
+
+        int intPlano=getResources().getIdentifier("drawable/mapabasso", null, this.getPackageName());
+        plano.setImageResource(intPlano);
+    }
+
+    public void googleMaps (View view){
+        Intent mapsIntent=new Intent(Intent.ACTION_VIEW, Uri.parse("https://www.google.com.ar/maps/@-32.3727404,-58.8760139,15z?hl=es"));
+        startActivity(mapsIntent);
     }
 }
