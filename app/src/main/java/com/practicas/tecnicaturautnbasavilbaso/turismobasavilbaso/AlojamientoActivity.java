54d28ac23c29c9850bb747855ef39b452d2777f7
GabrielNumaX <>
2019-03-14 15:26:19 -0300

    Logica Alojamiento y Gastronomia
    se hizo la logica de Alojamientos y Gastronomia y se la dejo comentada para asociar los id de las vistas.
    Tambien se agregaron strings

--- app/src/main/java/com/practicas/tecnicaturautnbasavilbaso/turismobasavilbaso/AlojamientoActivity.java
+++ app/src/main/java/com/practicas/tecnicaturautnbasavilbaso/turismobasavilbaso/AlojamientoActivity.java
@@ -2,12 +2,97 @@ package com.practicas.tecnicaturautnbasavilbaso.turismobasavilbaso;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class AlojamientoActivity extends AppCompatActivity {
+
+    ImageButton call5Soles;
+    ImageButton callRobertito;
+    ImageButton callDelOeste;
+    ImageButton callMBurucuya;
+    ImageButton callElTala;
+    ImageButton callElNe;
+    ImageButton callBungTermas;
+    ImageButton callCampPoli;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_alojamiento);
+
+    //call5Soles(ImageButton)=findViewById(); falta el R.id
+    //callRobertito(ImageButton)=findViewById(); falta el R.id
+    //callDelOeste(ImageButton)=findViewById(); falta el R.id.
+    //callMBurucuya(ImageButton)=findViewById(); falta el R.id.
+    //callElTala(ImageButton)=findViewById(); falta el R.id.
+    //callElNe(ImageButton)=findViewById(); falta el R.id.
+    //callBungTermas(ImageButton)=findViewById(); falta el R.id.
+    //callCampPoli(ImageButton)=findViewById(); falta el R.id.
     }
+
+    /*metodos de llamada
+    public void llamar5Soles (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445645410"));
+
+        startActivity(intent);
+    }
+
+    public void llamarRobertito (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445451590"));
+
+        startActivity(intent);
+    }
+
+    public void llamarDelOeste (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445481459"));
+
+        startActivity(intent);
+    }
+
+    public void llamarMBurucuya (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445654036"));
+
+        startActivity(intent);
+    }
+
+    public void llamarElTala (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445653910"));
+
+        startActivity(intent);
+    }
+
+    public void llamarElNe (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445458315"));
+
+        startActivity(intent);
+    }
+
+    public void llamarBungTermas (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445430083"));
+
+        startActivity(intent);
+    }
+
+    public void llamarCampPoli (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445481380"));
+
+        startActivity(intent);
+    }
+     */
 }
--- app/src/main/java/com/practicas/tecnicaturautnbasavilbaso/turismobasavilbaso/GastronomiaActivity.java
+++ app/src/main/java/com/practicas/tecnicaturautnbasavilbaso/turismobasavilbaso/GastronomiaActivity.java
@@ -2,12 +2,119 @@ package com.practicas.tecnicaturautnbasavilbaso.turismobasavilbaso;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageButton;
 
 public class GastronomiaActivity extends AppCompatActivity {
+
+    ImageButton callLaChacra;
+    ImageButton callClemente;
+    ImageButton callRobertito;
+    ImageButton callElBar;
+    ImageButton callBahamas;
+    ImageButton callLaOchava;
+    ImageButton callPipols;
+    ImageButton callHilda;
+    ImageButton callBobe;
+    ImageButton callSara;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gastronomia);
+
+        /*casteo botones
+        callLaChacra(ImageButton)=findViewById(R.id);
+        callClemente(ImageButton)=findViewById(R.id);
+        callRobertito(ImageButton)=findViewById(R.id);
+        callElBar(ImageButton)=findViewById(R.id);
+        callBahamas(ImageButton)=findViewById(R.id);
+        callLaOchava(ImageButton)=findViewById(R.id);
+        callPipols(ImageButton)=findViewById(R.id);
+        callHilda(ImageButton)=findViewById(R.id);
+        callBobe(ImageButton)=findViewById(R.id);
+        callSara(ImageButton)=findViewById(R.id);
+         */
     }
+
+    /*metodos llamadas
+    public void llamarLaChacra (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445419896"));
+
+        startActivity(intent);
+    }
+
+    public void llamarClemente (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445451247"));
+
+        startActivity(intent);
+    }
+
+    public void llamarRobertito (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445481590"));
+
+        startActivity(intent);
+    }
+
+    public void llamarElBar (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445482611"));
+
+        startActivity(intent);
+    }
+
+    public void llamarBahamas (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445482387"));
+
+        startActivity(intent);
+    }
+
+    public void llamarLaOchava (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445481853"));
+
+        startActivity(intent);
+    }
+
+    public void llamarPipols (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445453973"));
+
+        startActivity(intent);
+    }
+
+    public void llamarHilda (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445440045"));
+
+        startActivity(intent);
+    }
+
+    public void llamarBobe (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445456703"));
+
+        startActivity(intent);
+    }
+
+    public void llamarSara (View view) {
+
+        Intent intent = new Intent(Intent.ACTION_DIAL);
+        intent.setData(Uri.parse("tel:+543445481023"));
+
+        startActivity(intent);
+    }
+     */
 }
